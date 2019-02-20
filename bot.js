3dbc15941b1e8824479887f39f621ea1211c5d3b
YehezkielSaputra <yehezkiel.saputra@hotmail.com>
2019-02-20 10:44:16 +0700

    Add feature showing card sample on chat bot

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 /* Hardy Saputra - Call Levels */
 
-const { ActivityTypes } = require('botbuilder');
-const { DialogSet, TextPrompt, WaterfallDialog } = require('botbuilder-dialogs');
+const { ActivityTypes, CardFactory, AttachmentLayoutTypes } = require('botbuilder');
+const { DialogSet, TextPrompt, WaterfallDialog, ChoicePrompt, ListStyle } = require('botbuilder-dialogs');
 
 const { SlotFillingDialog } = require('./slotFillingDialog');
 const { SlotDetails } = require('./slotDetails');
@@ -21,6 +21,10 @@ class SampleBot {
 
         // Create a dialog set to include the dialogs used by this bot.
         this.dialogs = new DialogSet(this.dialogState);
+
+        const prompt = new ChoicePrompt('cardPrompt');
+        prompt.style = ListStyle.list;
+        this.dialogs.add(prompt);
 
         // Set up a series of questions for collecting the user's name.
         const fullnameSlots = [
@@ -81,6 +85,48 @@ class SampleBot {
         return await step.endDialog();
     }
 
+    async sendCardResponse(turnContext, dialogTurnResult) {
+        switch (dialogTurnResult.result.value) {
+        case 'Animation Card':
+            await turnContext.sendActivity({ attachments: [this.createAnimationCard()] });
+            break;
+        case 'Audio Card':
+            await turnContext.sendActivity({ attachments: [this.createAudioCard()] });
+            break;
+        case 'Hero Card':
+            await turnContext.sendActivity({ attachments: [this.createHeroCard()] });
+            break;
+        case 'Receipt Card':
+            await turnContext.sendActivity({ attachments: [this.createReceiptCard()] });
+            break;
+        case 'Signin Card':
+            await turnContext.sendActivity({ attachments: [this.createSignInCard()] });
+            break;
+        case 'Thumbnail Card':
+            await turnContext.sendActivity({ attachments: [this.createThumbnailCard()] });
+            break;
+        case 'Video Card':
+            await turnContext.sendActivity({ attachments: [this.createVideoCard()] });
+            break;
+        case 'All Cards':
+            await turnContext.sendActivity({
+                attachments: [this.createVideoCard(),
+                    this.createAnimationCard(),
+                    this.createAudioCard(),
+                    this.createHeroCard(),
+                    this.createReceiptCard(),
+                    this.createSignInCard(),
+                    this.createThumbnailCard(),
+                    this.createVideoCard()
+                ],
+                attachmentLayout: AttachmentLayoutTypes.Carousel
+            });
+            break;
+        default:
+            await turnContext.sendActivity('An invalid selection was parsed. No corresponding Rich Cards were found.');
+        }
+    }
+
     /**
      *
      * @param {TurnContext} turnContext A TurnContext object representing an incoming message to be handled by the bot.
@@ -91,6 +137,46 @@ class SampleBot {
             const dc = await this.dialogs.createContext(turnContext);
 
             const utterance = (turnContext.activity.text || '').trim().toLowerCase();
+
+            if (utterance === 'card') {
+                const promptOptions = {
+                    prompt: 'Please select a card:',
+                    retryPrompt: 'That was not a valid choice, please select a card or number from 1 to 8.',
+                    choices: this.getChoices()
+                };
+
+                await turnContext.sendActivity('Welcome to the Rich Cards Bot!');
+                await dc.prompt('cardPrompt', promptOptions);
+            }
+
+            if (utterance === 'animation') {
+                await turnContext.sendActivity({ attachments: [this.createAnimationCard()] });
+            }
+
+            if (utterance === 'audio') {
+                await turnContext.sendActivity({ attachments: [this.createAudioCard()] });
+            }
+
+            if (utterance === 'hero') {
+                await turnContext.sendActivity({ attachments: [this.createHeroCard()] });
+            }
+
+            if (utterance === 'receipt') {
+                await turnContext.sendActivity({ attachments: [this.createReceiptCard()] });
+            }
+
+            if (utterance === 'signin') {
+                await turnContext.sendActivity({ attachments: [this.createSignInCard()] });
+            }
+
+            if (utterance === 'thumbnail') {
+                await turnContext.sendActivity({ attachments: [this.createThumbnailCard()] });
+            }
+
+            if (utterance === 'video') {
+                await turnContext.sendActivity({ attachments: [this.createVideoCard()] });
+            }
+
             if (utterance === 'cancel') {
                 if (dc.activeDialog) {
                     await dc.cancelAllDialogs();
@@ -122,6 +208,173 @@ class SampleBot {
         }
 
         await this.conversationState.saveChanges(turnContext);
+    }
+
+    /**
+     * Create the choices with synonyms to render for the user during the ChoicePrompt.
+     */
+    getChoices() {
+        const cardOptions = [
+            {
+                value: 'Animation Card',
+                synonyms: ['1', 'animation', 'animation card']
+            },
+            {
+                value: 'Audio Card',
+                synonyms: ['2', 'audio', 'audio card']
+            },
+            {
+                value: 'Hero Card',
+                synonyms: ['3', 'hero', 'hero card']
+            },
+            {
+                value: 'Receipt Card',
+                synonyms: ['4', 'receipt', 'receipt card']
+            },
+            {
+                value: 'Signin Card',
+                synonyms: ['5', 'signin', 'signin card']
+            },
+            {
+                value: 'Thumbnail Card',
+                synonyms: ['6', 'thumbnail', 'thumbnail card']
+            },
+            {
+                value: 'Video Card',
+                synonyms: ['7', 'video', 'video card']
+            },
+            {
+                value: 'All Cards',
+                synonyms: ['8', 'all', 'all cards']
+            }
+        ];
+
+        return cardOptions;
+    }
+
+    createAnimationCard() {
+        return CardFactory.animationCard(
+            'Microsoft Bot Framework',
+            [
+                { url: 'https://i.giphy.com/Ki55RUbOV5njy.gif' }
+            ],
+            [],
+            {
+                subtitle: 'Animation Card'
+            }
+        );
+    }
+
+    createAudioCard() {
+        return CardFactory.audioCard(
+            'I am your father',
+            ['https://www.mediacollege.com/downloads/sound-effects/star-wars/darthvader/darthvader_yourfather.wav'],
+            CardFactory.actions([
+                {
+                    type: 'openUrl',
+                    title: 'Read more',
+                    value: 'https://en.wikipedia.org/wiki/The_Empire_Strikes_Back'
+                }
+            ]),
+            {
+                subtitle: 'Star Wars: Episode V - The Empire Strikes Back',
+                text: 'The Empire Strikes Back (also known as Star Wars: Episode V – The Empire Strikes Back) is a 1980 American epic space opera film directed by Irvin Kershner. Leigh Brackett and Lawrence Kasdan wrote the screenplay, with George Lucas writing the film\'s story and serving as executive producer. The second installment in the original Star Wars trilogy, it was produced by Gary Kurtz for Lucasfilm Ltd. and stars Mark Hamill, Harrison Ford, Carrie Fisher, Billy Dee Williams, Anthony Daniels, David Prowse, Kenny Baker, Peter Mayhew and Frank Oz.',
+                image: 'https://upload.wikimedia.org/wikipedia/en/3/3c/SW_-_Empire_Strikes_Back.jpg'
+            }
+        );
+    }
+
+    createHeroCard() {
+        return CardFactory.heroCard(
+            'BotFramework Hero Card',
+            CardFactory.images(['https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg']),
+            CardFactory.actions([
+                {
+                    type: 'openUrl',
+                    title: 'Get started',
+                    value: 'https://docs.microsoft.com/en-us/azure/bot-service/'
+                }
+            ])
+        );
+    }
+
+    createReceiptCard() {
+        return CardFactory.receiptCard({
+            title: 'John Doe',
+            facts: [
+                {
+                    key: 'Order Number',
+                    value: '1234'
+                },
+                {
+                    key: 'Payment Method',
+                    value: 'VISA 5555-****'
+                }
+            ],
+            items: [
+                {
+                    title: 'Data Transfer',
+                    price: '$38.45',
+                    quantity: 368,
+                    image: { url: 'https://github.com/amido/azure-vector-icons/raw/master/renders/traffic-manager.png' }
+                },
+                {
+                    title: 'App Service',
+                    price: '$45.00',
+                    quantity: 720,
+                    image: { url: 'https://github.com/amido/azure-vector-icons/raw/master/renders/cloud-service.png' }
+                }
+            ],
+            tax: '$7.50',
+            total: '$90.95',
+            buttons: CardFactory.actions([
+                {
+                    type: 'openUrl',
+                    title: 'More information',
+                    value: 'https://azure.microsoft.com/en-us/pricing/details/bot-service/'
+                }
+            ])
+        });
+    }
+
+    createSignInCard() {
+        return CardFactory.signinCard(
+            'BotFramework Sign in Card',
+            'https://login.microsoftonline.com',
+            'Sign in'
+        );
+    }
+
+    createThumbnailCard() {
+        return CardFactory.thumbnailCard(
+            'BotFramework Thumbnail Card',
+            [{ url: 'https://sec.ch9.ms/ch9/7ff5/e07cfef0-aa3b-40bb-9baa-7c9ef8ff7ff5/buildreactionbotframework_960.jpg' }],
+            [{
+                type: 'openUrl',
+                title: 'Get started',
+                value: 'https://docs.microsoft.com/en-us/azure/bot-service/'
+            }],
+            {
+                subtitle: 'Your bots — wherever your users are talking.',
+                text: 'Build and connect intelligent bots to interact with your users naturally wherever they are, from text/sms to Skype, Slack, Office 365 mail and other popular services.'
+            }
+        );
+    }
+
+    createVideoCard() {
+        return CardFactory.videoCard(
+            '2018 Imagine Cup World Championship Intro',
+            [{ url: 'https://sec.ch9.ms/ch9/783d/d57287a5-185f-4df9-aa08-fcab699a783d/IC18WorldChampionshipIntro2.mp4' }],
+            [{
+                type: 'openUrl',
+                title: 'Lean More',
+                value: 'https://channel9.msdn.com/Events/Imagine-Cup/World-Finals-2018/2018-Imagine-Cup-World-Championship-Intro'
+            }],
+            {
+                subtitle: 'by Microsoft',
+                text: 'Microsoft\'s Imagine Cup has empowered student developers around the world to create and innovate on the world stage for the past 16 years. These innovations will shape how we live, work and play.'
+            }
+        );
     }
 }
 
