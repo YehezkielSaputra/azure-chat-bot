28494acdcfbeba00f3f7948eb3b010386c40a50c
YehezkielSaputra <yehezkiel.saputra@hotmail.com>
2019-02-25 14:22:21 +0700

    Clean code remove comment

--- bot.js
+++ bot.js
@@ -19,10 +19,8 @@ class SampleBot {
     }
     async logic(conversationState) {
         this.conversationState = conversationState;
-        // Create a property used to store dialog state.
         this.dialogState = this.conversationState.createProperty(DIALOG_STATE_PROPERTY);
 
-        // Create a dialog set to include the dialogs used by this bot.
         this.dialogs = new DialogSet(this.dialogState);
 
         this.dialogs.add(new TextPrompt('text'));
@@ -31,8 +29,6 @@ class SampleBot {
         var flightOrder = new FlightOrder(this.dialogs);
         var confirmation = new Confirmation(this.dialogs);
 
-        // Finally, add a 2-step WaterfallDialog that will initiate the SlotFillingDialog,
-        // and then collect and display the results.
         this.dialogs.add(new WaterfallDialog('root', [
             this.startDialog.bind(this),
             userDetail.userDialog.bind(this),
@@ -40,11 +36,8 @@ class SampleBot {
             confirmation.confirmDialog.bind(this)
         ]));
     }
-    // This is the first step of the WaterfallDialog.
-    // It kicks off the dialog with the multi-question SlotFillingDialog,
-    // then passes the aggregated results on to the next step.
+
     async startDialog(step) {
-        // return await step.beginDialog('confirm-slot');
         return await step.beginDialog('detailUser-slot');
     }
 
@@ -54,23 +47,19 @@ class SampleBot {
      */
     async onTurn(turnContext) {
         if (turnContext.activity.type === ActivityTypes.Message) {
-            // Create dialog context.
             const dc = await this.dialogs.createContext(turnContext);
 
             if (!dc.context.responded) {
-                // Continue the current dialog if one is pending.
                 await dc.continueDialog();
             }
 
             if (!dc.context.responded) {
-                // If no response has been sent, start the onboarding dialog.
                 await dc.beginDialog('root');
             }
         } else if (
             turnContext.activity.type === ActivityTypes.ConversationUpdate &&
             turnContext.activity.membersAdded[0].name !== 'Bot'
         ) {
-            // Send a "this is what the bot does" message.
             const description = [
                 'This is a bot that demonstrates to collect multiple responses from a user.',
                 'Say anything to continue.'
